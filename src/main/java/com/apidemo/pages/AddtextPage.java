5eb921c3f84baa175e0601058ba09625aeb078ca
Sounak Roy <sounak.roy@nagarro.com>
2022-03-15 12:50:53 +0530

    minor changes

--- src/main/java/com/apidemo/pages/AddtextPage.java
+++ src/main/java/com/apidemo/pages/AddtextPage.java
@@ -2,13 +2,11 @@ package com.apidemo.pages;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import com.apidemo.utitlity.Explicitwait;
 
